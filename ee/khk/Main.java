ee5d4c71ec6c408b5969beb80baa8237ffd1b10d
Mario Jõelo <joelo.mario@khk.ee>
2021-03-23 12:11:48 +0200

    1.3.2 Scene klass button

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -1,23 +1,27 @@
 package ee.khk;
 
 import javafx.application.Application;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
 import javafx.scene.text.Text;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class Main extends Application{
     public static void main(String[] args){
         Application.launch(args);
     }
     public void start(Stage stage) {
-        Group root = new Group();
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
+        Label label = new Label("Hello");
+        Button button = new Button("Button");
+        Group group = new Group(button);
 
+        FlowPane root = new FlowPane(label, group);
+        Scene scene = new Scene(root, 300, 150);
+        stage.setScene(scene);
         stage.setTitle("Hello JavaFX");
-        stage.setWidth(300);
-        stage.setHeight(150);
         stage.show();
     }
 }
