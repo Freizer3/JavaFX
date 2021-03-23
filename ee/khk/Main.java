2e239e5644549a4bf719472581e1ae7a70f8fac5
Mario Jõelo <joelo.mario@khk.ee>
2021-03-23 12:06:09 +0200

    1.2 Stage klass

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -8,20 +8,13 @@ import javafx.scene.text.Text;
 
 public class Main extends Application{
     public static void main(String[] args){
-        System.out.println("Launching Application");
         Application.launch(args);
     }
-    public void init() throws Exception {
-        System.out.println("Application inits");
-    super.init();
-    }
     public void start(Stage stage) {
-        System.out.println("Application starts");
+        stage.setTitle("Hello JavaFX");
+        stage.setWidth(250);
+        stage.setHeight(150);
         stage.show();
-    }
-    public void stop() throws Exception {
-        System.out.println("Application stops");
-        super.stop();
     }
 }
 
