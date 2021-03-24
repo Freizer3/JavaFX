1d0f5985407470a7ee640e3b52f858fbd54a524a
Mario Jõelo <joelo.mario@khk.ee>
2021-03-24 13:54:13 +0200

    3.6.2 Stackpane

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -27,7 +27,9 @@ public class Main extends Application{
     @Override
     public void start(Stage stage) {
         Label headerLbl = new Label("Press the login button");
+        StackPane.setAlignment(headerLbl, Pos.TOP_CENTER);
         Label statusLbl = new Label("Start screen");
+        StackPane.setAlignment(statusLbl, Pos.BOTTOM_CENTER);
         Button loginBtn = new Button("Login");
 
         StackPane root = new StackPane(headerLbl, statusLbl, loginBtn);
