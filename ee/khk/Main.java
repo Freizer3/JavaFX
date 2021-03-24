2adf424e3da32c3bea7810c0a1d2e912f0d840f5
Mario Jõelo <joelo.mario@khk.ee>
2021-03-24 13:55:35 +0200

    3.6.3 Stackpane

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
@@ -28,8 +29,12 @@ public class Main extends Application{
     public void start(Stage stage) {
         Label headerLbl = new Label("Press the login button");
         StackPane.setAlignment(headerLbl, Pos.TOP_CENTER);
+        StackPane.setMargin(headerLbl, new Insets(10,0,0,0));
+
         Label statusLbl = new Label("Start screen");
         StackPane.setAlignment(statusLbl, Pos.BOTTOM_CENTER);
+        StackPane.setMargin(statusLbl, new Insets(0,0,10,0));
+
         Button loginBtn = new Button("Login");
 
         StackPane root = new StackPane(headerLbl, statusLbl, loginBtn);
