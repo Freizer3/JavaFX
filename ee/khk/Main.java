962891f09975d5d25b1f905df6be9df16094e8ac
Mario Jõelo <joelo.mario@khk.ee>
2021-03-24 13:35:51 +0200

    3.3 HBox

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -8,6 +8,7 @@ import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -16,6 +17,7 @@ import javafx.scene.text.Text;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.Parent;
+
 
 
 public class Main extends Application{
@@ -27,15 +29,18 @@ public class Main extends Application{
     public void start(Stage stage) {
         Label label1 = new Label("Label1");
         Label label2 = new Label("Label2");
+        Label label3 = new Label("Label3");
+        Label label4 = new Label("Label4");
+        Label label5 = new Label("Label5");
+        Label label6 = new Label("Label6");
+        Label label7 = new Label("Label7");
 
+        HBox root = new HBox(10, label1, label2, label3, label4, label5, label6, label7);
 
-        FlowPane root = new FlowPane(10, 10, label1, label2);
-        root.setAlignment(Pos.CENTER);
-
-        Scene scene = new Scene(root, 300, 150);
+        Scene scene = new Scene(root, 500, 150);
         stage.setScene(scene);
 
-        stage.setTitle("FlowPane in JavaFX");
+        stage.setTitle("HBox in JavaFX");
 
         stage.show();
     }
